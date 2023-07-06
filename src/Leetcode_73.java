089e86de8b0da3d77f81f2c35c3b9e21d5dc14c2
“PRATEEK <mukeshkumarsingh.4462@gmail.com>
2023-07-07 00:19:26 +0530

    second commit

--- src/Leetcode_73.java
+++ src/Leetcode_73.java
@@ -66,13 +66,29 @@ class Leetcode_73 {
 
         public static void main(String[] args) {
             System.out.println("input array: ");
-            int[][] matrix = new int[][]{{0,1,2,0}, {3,4,5,2}, {1,3,1,5}};
+            int[][] matrix = new int[][]{{1,0,2,0}, {3,4,5,2}, {1,3,0,5}};
+
             for (int[] print:matrix){
                 System.out.println(Arrays.toString(print));
             }
+            System.out.println("\n");
             Leetcode_73 l=new Leetcode_73();
             l.setZeroes(matrix);
 
         }
     }
 
+
+//    The time complexity of the given code is O(m * n), where m is the number of rows in the matrix and n is the number of columns.
+//
+//        The code consists of several loops that iterate over the matrix elements. Let's analyze each part of the code:
+//
+//        The first two loops iterate over the first row and the first column of the matrix, respectively. These loops have a complexity of O(n) and O(m) each.
+//
+//        The nested loops starting from the third loop iterate over the non-marker elements of the matrix. These loops iterate m-1 * n-1 times, which gives a complexity of O((m-1) * (n-1)).
+//
+//        The next two loops iterate over the rows and columns of the matrix, excluding the first row and column. These loops iterate (m-1) * n and m * (n-1) times, respectively, resulting in a complexity of O((m-1) * n) and O(m * (n-1)).
+//
+//        The last two loops iterate over the first column and the first row of the matrix, respectively. These loops have a complexity of O(m) and O(n) each.
+//
+//        Overall, the dominant factor in terms of time complexity is the loop that iterates over the non-marker elements of the matrix, which has a complexity of O((m-1) * (n-1)). Therefore, the overall time complexity of the code is O(m * n) since the other parts of the code have lower order complexity.
