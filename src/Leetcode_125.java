d1977826e16731d53b85d686b13bf9ea766f57e0
“PRATEEK <mukeshkumarsingh.4462@gmail.com>
2023-07-21 00:20:14 +0530

    leetcode 9,125,509 added

---
+++ src/Leetcode_125.java
@@ -0,0 +1,19 @@
+public class Leetcode_125 {
+    public static void main(String[] args) {
+        String s="A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s));
+    }
+    static boolean isPalindrome(String n){
+        String original=n.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        String rev = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            rev += original.charAt(i);
+        }
+        if (original.equals(rev)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ src/Leetcode_9.java
@@ -0,0 +1,16 @@
+public class Leetcode_9 {
+    public static void main(String[] args) {
+        int x = 99;
+        String original = Integer.toString(x);
+        String rev = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            rev += original.charAt(i);
+        }
+        if (original.equals(rev)) {
+            System.out.println("PALINDROME");
+        } else {
+            System.out.println("NON PALINDROME");
+        }
+    }
+
+}
