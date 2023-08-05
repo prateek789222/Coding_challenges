081af1a4538760f7b857d03011f36c9f791a7d99
“PRATEEK <mukeshkumarsingh.4462@gmail.com>
2023-08-05 15:24:04 +0530

    leetcode 1342 added

---
+++ src/Leetcode_1342.java
@@ -0,0 +1,17 @@
+public class Leetcode_1342 {
+    public static int numberOfSteps(int num) {
+        return helper(num,0);
+    }
+    static int helper(int num,int count){
+        if(num==0)
+            return count;
+        if(num%2==0)
+            return helper(num/2,count+1);
+        else
+            return helper(num-1,count+1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numberOfSteps(123));
+    }
+}
